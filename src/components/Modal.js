c279e68facc0bec915dead0eb42a2adbd2ce1bac
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2021-11-02 13:27:06 -0500

    add bootstrap styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Modal, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Example() {
     const [show, setShow] = useState(false);
